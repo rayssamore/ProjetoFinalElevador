b82269ad91ccd3a268babaf632f8eae5851cbc4c
rayssamore <rayssagm_@hotmail.com>
2022-06-03 23:54:19 -0300

    projeto final

---
+++ Models/Elevador.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NmElevador.Models
+{
+	public class Programa
+	{
+
+
+		//Classe Elevador
+		public class Elevador
+		{
+			//Definindo variaveis globais para pessoas
+			public  class PElevador
+			{
+				public static int capacidadePessoasElevador;
+				public static int pessoasElevador;
+				public static int andarAtual;
+			}
+			//Definindo variaveis globais para o prédio
+			public static class CMaxPredio
+			{
+				public static int qtdAndares;
+
+			}
+
+			//Definindo método para inicializar e obter valores da capacidade de pessoas e qtd de andares do prédio
+			public void Inicializar(int capacidadePessoasElevador,int qtdAndares)
+			{
+				//int andarElevador = 0;
+				PElevador.pessoasElevador = 0;
+				PElevador.andarAtual = 0;
+			}
+			//Definindo método para inserir pessoas no elevador considerando a quantidade máxima permitida
+			public int Entrar(int qtdAndares, int capacidadePessoasElevador, int pessoasElevador)
+			{
+				if (pessoasElevador  < capacidadePessoasElevador)
+				{
+					PElevador.pessoasElevador ++;
+					//int pessoasTotal =+ 1;
+				}
+				else
+				{
+					Console.WriteLine("O elevador atingiu a capacidade maxima de pessoas");
+				}
+				return PElevador.pessoasElevador;
+
+			}
+			//Definindo método para retirar pessoas no elevador considerando a quantidade mínima permitida
+			public int Sair(int pessoasTotal)
+			{
+				if (pessoasTotal != 0)
+				{
+					Elevador.PElevador.pessoasElevador --;
+					Console.WriteLine("Total de pessoas no elevador: " + pessoasTotal);
+				}
+				else
+				{
+					Console.WriteLine("Não existe ninguém no elevador!");
+					
+				}
+				return PElevador.pessoasElevador;
+			}
+			//Definindo método para subir andares do prédio considerando o andar atual para iterar e a quantidade máxima permitida
+			public int subirAndar(int capacidadeMax, int andarAtual)
+			{
+				if (andarAtual < capacidadeMax)
+				{
+					PElevador.andarAtual ++; 
+					Console.WriteLine("Você está no " + PElevador.andarAtual + " andar");
+				}
+				else
+				{
+					Console.WriteLine("Você está tentando subir mais do que a quantidade de andares do prédio.");
+				} return andarAtual;
+
+			}
+			//Definindo método para descer andares do prédio considerando o andar atual para diminuir e a quantidade mínima permitida
+			public void descerAndar(int andarAtual)
+			{
+				if (andarAtual != 0 && andarAtual > 0)
+				{
+					Elevador.PElevador.andarAtual --;
+					Console.WriteLine("Você está no " + PElevador.andarAtual + " andar");
+				}
+				else
+				{
+					Console.WriteLine("Não é possível descer mais andares do que o térreo.");
+
+				}
+			}
+
+
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using static NmElevador.Models.Programa;
+
+namespace ProjetoElevador
+{
+    internal class Program
+    {
+        public static class tipos
+        {
+            public static int pessoasTotal;
+            public static int andarAtual;
+
+        }
+        static void Main(string[] args)
+        {
+            //Definindo a capacidade máxima de pessoas permitidas no elevador e o total de andares no prédio
+            Elevador novoElevador = new Elevador();
+            Console.WriteLine("Digite a capacidade máxima de pessoas permitida no Elevador:");
+            Elevador.PElevador.capacidadePessoasElevador = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o total de andares do prédio:");
+            Elevador.CMaxPredio.qtdAndares = Convert.ToInt32(Console.ReadLine());
+
+            //Utilizando o método Inicializar
+            novoElevador.Inicializar(Elevador.PElevador.capacidadePessoasElevador, Elevador.CMaxPredio.qtdAndares);
+            bool continuar = true;
+            
+            do
+            {
+                //Definindo lista para auxiliar o usuário nas ações que ele pode utilizar
+                Console.WriteLine(@"Escolha uma opção:
+                        1 - Entrar
+                        2 - Subir
+                        3 - Sair
+                        4 - Descer
+                        5 - Sair do programa
+                        ");
+                string opcaoEscolhida = Console.ReadLine();
+
+                // Limpando o console
+                Console.Clear();
+                //Quando a opção escolhida for 1, o programa direciona para o método de entrar um usuário no elevador
+                if (opcaoEscolhida == "1")
+                {
+                    novoElevador.Entrar(Elevador.CMaxPredio.qtdAndares, Elevador.PElevador.capacidadePessoasElevador, Elevador.PElevador.pessoasElevador);
+
+
+                    Console.WriteLine("total de pessoas elevador : " + Elevador.PElevador.pessoasElevador);
+
+                }
+                //Quando a opção escolhida for 2, o programa direciona para o método para subir um andar no elevador
+                if (opcaoEscolhida == "2")
+                {
+                    novoElevador.subirAndar(Elevador.CMaxPredio.qtdAndares, Elevador.PElevador.andarAtual);
+                    //tipos.andarAtual = +1;
+                }
+                //Quando a opção escolhida for 3, o programa direciona para o método para sair um usuário do elevador
+                if (opcaoEscolhida == "3")
+                {
+                    novoElevador.Sair(Elevador.PElevador.pessoasElevador);
+
+                }
+                //Quando a opção escolhida for 4, o programa direciona para o método para descer um andar no elevador
+                if (opcaoEscolhida == "4")
+                {
+                    novoElevador.descerAndar(Elevador.PElevador.andarAtual);
+                   
+                }
+                //Quando a opção escolhida for 5, o programa direciona para sair do programa
+                if (opcaoEscolhida == "5")
+                {
+                    continuar = false;
+                }
+
+
+            }
+
+            while (continuar);
+
+        }
+
+    
+
+
+
+    }
+
+}
+
